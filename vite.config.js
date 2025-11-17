2bc16d6ed9cfc930dcfe73dd138e4829f48b9ce5
Mathieu Joubin <mathieu.joubin@digidrive.fr>
2025-11-17 16:20:13 +0100

    bump deps

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
